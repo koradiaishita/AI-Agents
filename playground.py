ea5ddf45f46a30a4783f015265a7f2ec4017ad33
koradiaishita <koradiaishita@gmail.com>
2025-02-22 16:08:19 +0000

    Add finance and web agents with integrated tools and storage

---
+++ playground.py
@@ -0,0 +1,33 @@
+from phi.agent import Agent
+from phi.model.google import Gemini
+from phi.storage.agent.sqlite import SqlAgentStorage
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+from phi.playground import Playground, serve_playground_app
+
+web_agent = Agent(
+    name=" Yash Web Agent",
+    model=Gemini(id="gemini-1.5-flash"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    storage=SqlAgentStorage(table_name="web_agent", db_file="agents.db"),
+    add_history_to_messages=True,
+    markdown=True,
+)
+
+finance_agent = Agent(
+    name="Ishita Finance Agent",
+    model=Gemini(id="gemini-1.5-flash"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
+    storage=SqlAgentStorage(table_name="finance_agent", db_file="agents.db"),
+    add_history_to_messages=True,
+    markdown=True,
+)
+
+app = Playground(agents=[finance_agent, web_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
+
+    
